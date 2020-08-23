a5ce488eda68ee3470ee1b27b0a01c7dc6988d5d
梁书豪 <shuhao1229@163.com>
2020-08-24 00:42:30 +0800

    横向列表开发，点击item 高亮显示，横向列表滚动

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import { categoryTypes, alphaTypes } from '../../api/config';
 import { NavContainer } from './style';
 
+// item 样式随当前索引值变化 
 function Singers (props) {
+
+  let [category, setCategory] = useState('');;
+  let [alpha, setAlpha] = useState('');
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha (val);  // "A" "B"
+  }
+  let handleUpdateCatetory = (val) => {
+    setCategory (val); // 1001 1002
+  }
+
   return (
     <NavContainer>
-      <Horizen list={categoryTypes} title={"分类（默认热门）:"}></Horizen>
-      <Horizen list={alphaTypes} title={"首字母:"}></Horizen>
+      <Horizen 
+        list={categoryTypes} 
+        title={"分类（默认热门）:"}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}
+        ></Horizen>
+      <Horizen 
+        list={alphaTypes} 
+        title={"首字母:"}
+        handleClick={val => handleUpdateAlpha(val)}
+        oldVal={alpha}
+        ></Horizen>
     </NavContainer>
   )
 }
--- src/application/Singers/style.js
+++ src/application/Singers/style.js
@@ -1,6 +1,9 @@
 import styled from'styled-components';
 import style from '../../assets/global-style';
 
+// todo 解决横向列表的滚动问题 添加一个 NavContainer 容器，宽度100%
+// 1. 外部容器未限定宽度，也就是两个 Horizen 外面包裹的 div 元素。
+// 2. 内部宽度没有进行相应的计算，始终为屏幕宽度。
 export const NavContainer  = styled.div`
   box-sizing: border-box;
   position: fixed;
