7f1eb6575606a7ea425c93d667fa5b9b975ce2f8
梁书豪 <shuhao1229@163.com>
2020-08-24 00:53:36 +0800

    歌手列表 mock数据完成 滚动歌手列表效果实现

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import { categoryTypes, alphaTypes } from '../../api/config';
-import { NavContainer } from './style';
+import { 
+  NavContainer,
+  ListContainer,
+  List,
+  ListItem,
+} from "./style";
+import Scroll from '../../baseUI/scroll';
+
 
 // item 样式随当前索引值变化 
 function Singers (props) {
@@ -16,21 +23,57 @@ function Singers (props) {
     setCategory (val); // 1001 1002
   }
 
+  //mock 数据
+  const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map (item => {
+    return {
+      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+      name: "张杰勇敢飞",
+      accountId: 277313426,
+    }
+  }); 
+
+  // 渲染函数，返回歌手列表
+  const renderSingerList = () => {
+    return (
+      <List>
+        {
+          singerList.map ((item, index) => {
+            return (
+              <ListItem key={item.accountId+""+index}>
+                <div className="img_wrapper">
+                  <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                </div>
+                <span className="name">{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+      </List>
+    )
+  };
+
   return (
-    <NavContainer>
-      <Horizen 
-        list={categoryTypes} 
-        title={"分类（默认热门）:"}
-        handleClick={handleUpdateCatetory}
-        oldVal={category}
-        ></Horizen>
-      <Horizen 
-        list={alphaTypes} 
-        title={"首字母:"}
-        handleClick={val => handleUpdateAlpha(val)}
-        oldVal={alpha}
-        ></Horizen>
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horizen 
+          list={categoryTypes} 
+          title={"分类（默认热门）:"}
+          handleClick={handleUpdateCatetory}
+          oldVal={category}
+          ></Horizen>
+        <Horizen 
+          list={alphaTypes} 
+          title={"首字母:"}
+          handleClick={val => handleUpdateAlpha(val)}
+          oldVal={alpha}
+          ></Horizen>
+      </NavContainer>
+      <ListContainer>
+        <Scroll>
+          {renderSingerList()}
+        </Scroll>
+      </ListContainer>
+    </div>
   )
 }
 
