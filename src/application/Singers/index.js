6e5db9f8f25f441fea7330cf2400ee601215ff7d
梁书豪 <shuhao1229@163.com>
2020-08-24 12:19:40 +0800

    歌手List开发，使用真实数据，选择对应分类歌手，实现更新更新列表功能

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import { categoryTypes, alphaTypes } from '../../api/config';
 import { 
@@ -8,6 +8,18 @@ import {
   ListItem,
 } from "./style";
 import Scroll from '../../baseUI/scroll';
+import { 
+  getSingerList, 
+  getHotSingerList, 
+  changeEnterLoading, 
+  changePageCount, 
+  refreshMoreSingerList, 
+  changePullUpLoading, 
+  changePullDownLoading, 
+  refreshMoreHotSingerList
+} from './store/actionCreators';
+import {connect} from 'react-redux';
+import  LazyLoad, {forceCheck} from 'react-lazyload';
 
 
 // item 样式随当前索引值变化 
@@ -16,32 +28,48 @@ function Singers (props) {
   let [category, setCategory] = useState('');;
   let [alpha, setAlpha] = useState('');
 
-  let handleUpdateAlpha = (val) => {
-    setAlpha (val);  // "A" "B"
-  }
-  let handleUpdateCatetory = (val) => {
-    setCategory (val); // 1001 1002
-  }
+  const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount } = props;
 
-  //mock 数据
-  const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map (item => {
-    return {
-      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
-      name: "张杰勇敢飞",
-      accountId: 277313426,
-    }
-  }); 
+  const { getHotSingerDispatch, updateDispatch, pullDownRefreshDispatch, pullUpRefreshDispatch } = props;
+
+  useEffect(() => {
+    getHotSingerDispatch();
+    // eslint-disable-next-line
+  }, []);
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val);
+    // 更新 字母栏
+    updateDispatch(category, val);
+  };
+
+  let handleUpdateCatetory = (val) => {
+    setCategory(val);
+    // 更新 歌手分类栏
+    updateDispatch(val, alpha);
+  };
+
+  const handlePullUp = () => {
+    pullUpRefreshDispatch(category, alpha, category === '', pageCount);
+  };
+
+  const handlePullDown = () => {
+    pullDownRefreshDispatch(category, alpha);
+  };
 
   // 渲染函数，返回歌手列表
   const renderSingerList = () => {
+    const list = singerList ? singerList.toJS() : [];
     return (
       <List>
         {
-          singerList.map ((item, index) => {
+          list.map ((item, index) => {
             return (
               <ListItem key={item.accountId+""+index}>
                 <div className="img_wrapper">
-                  <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./singer.png')} alt="music"/>}>
+                    <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                  </LazyLoad>
                 </div>
                 <span className="name">{item.name}</span>
               </ListItem>
@@ -57,16 +85,16 @@ function Singers (props) {
       <NavContainer>
         <Horizen 
           list={categoryTypes} 
-          title={"分类（默认热门）:"}
-          handleClick={handleUpdateCatetory}
+          title={"分类(默认热门):"} 
+          handleClick={(val) => handleUpdateCatetory(val)} 
           oldVal={category}
-          ></Horizen>
+        ></Horizen>
         <Horizen 
           list={alphaTypes} 
-          title={"首字母:"}
-          handleClick={val => handleUpdateAlpha(val)}
+          title={"首字母:"} 
+          handleClick={val => handleUpdateAlpha(val)} 
           oldVal={alpha}
-          ></Horizen>
+        ></Horizen>
       </NavContainer>
       <ListContainer>
         <Scroll>
@@ -77,4 +105,48 @@ function Singers (props) {
   )
 }
 
-export default React.memo(Singers);
+// 把 reducer中的状态映射成属性，通过容器组件传递到 Singers UI组件
+const mapStateToProps = (state) => ({
+  singerList: state.getIn(['singers', 'singerList']),
+  enterLoading: state.getIn(['singers', 'enterLoading']),
+  pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+  pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+  pageCount: state.getIn(['singers', 'pageCount'])
+});
+
+// 将 actionCreator中 异步获取来的数据 dispatch 到 reducer中 去修改
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getHotSingerDispatch() {
+      dispatch(getHotSingerList());
+    },
+    updateDispatch(category, alpha) {
+      dispatch(changePageCount(0));//由于改变了分类，所以pageCount清零
+      dispatch(changeEnterLoading(true));//loading，现在实现控制逻辑，效果实现放到下一节，后面的loading同理
+      dispatch(getSingerList(category, alpha));
+    },
+    // 滑到最底部刷新部分的处理
+    pullUpRefreshDispatch(category, alpha, hot, count) {
+      dispatch(changePullUpLoading(true));
+      dispatch(changePageCount(count+1));
+      if(hot){
+        dispatch(refreshMoreHotSingerList());
+      } else {
+        dispatch(refreshMoreSingerList(category, alpha));
+      }
+    },
+    //顶部下拉刷新
+    pullDownRefreshDispatch(category, alpha) {
+      dispatch(changePullDownLoading(true));
+      dispatch(changePageCount(0));//属于重新获取数据
+      if(category === '' && alpha === ''){
+        dispatch(getHotSingerList());
+      } else {
+        dispatch(getSingerList(category, alpha));
+      }
+
+    }
+  }
+};  
+
+export default connect(mapStateToProps, mapDispatchToProps)(Singers);
